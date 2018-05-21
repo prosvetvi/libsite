1583ddc9aa39520cab65697a86b7d468d9f9d7b5
root <root@root.com>
2018-05-21 19:07:15 +0300

    lib-28 api update book

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.views import ListBooksView
+from api.views import ListBooksView, BooksDetailView
 
 urlpatterns = [
-    path('books/', ListBooksView.as_view(), name="books-all")
+    path('books/', ListBooksView.as_view(), name="books-all"),
+    path('books/<int:pk>/edit', BooksDetailView.as_view(), name="book-update")
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from api.serializers import BooksSerializer
 from catalog.models import Book
@@ -10,3 +11,25 @@ class ListBooksView(generics.ListAPIView):
     """
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
+
+
+class BooksDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    PUT songs/:id/
+    """
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
+
+    def put(self, request, *args, **kwargs):
+        try:
+            a_book = self.queryset.get(pk=kwargs["pk"])
+            serializer = BooksSerializer()
+            updated_book = serializer.update(a_book, request.data)
+            return Response(BooksSerializer(updated_book).data)
+        except Book.DoesNotExist:
+            return Response(
+                data={
+                    "message": "Book with id: {} does not exist".format(kwargs["pk"])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
